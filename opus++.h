94b299de6ee4ef7e4a0943cb23ae735b85e510d0
Arto Bendiken <arto@dryproject.org>
2017-12-03 10:19:48 +0100

    Implemented opus::encoder and opus::decoder construction.

--- opus++.h
+++ opus++.h
@@ -4,7 +4,9 @@
 #define OPUSXX_H
 
 /**
- * <opus++.h>
+ * <opus++.h> - Public domain C++11 wrapper for the libopus <opus.h> interface.
+ *
+ * @author Arto Bendiken
  */
 
 #ifndef __cplusplus
@@ -19,12 +21,86 @@
 
 #include <opus.h> /* for libopus */
 
+#include <memory> /* for std::unique_ptr */
+
 ////////////////////////////////////////////////////////////////////////////////
 
 namespace opus {
   class encoder;
   class decoder;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @see https://mf4.xiph.org/jenkins/view/opus/job/opus/ws/doc/html/group__opus__encoder.html
+ */
+class opus::encoder {
+public:
+  using handle_ptr = std::unique_ptr<OpusEncoder, void(*)(OpusEncoder*)>;
+
+protected:
+  // @see https://mf4.xiph.org/jenkins/view/opus/job/opus/ws/doc/html/group__opus__encoder.html#ga5f4c05b4b51cdffec5a55dbf17bbfa1c
+  handle_ptr _handle{nullptr, opus_encoder_destroy};
+
+public:
+  explicit encoder() noexcept = default;
+
+  explicit encoder(OpusEncoder* const handle) noexcept
+    : _handle{handle, opus_encoder_destroy} {}
+
+  const OpusEncoder* handle() const noexcept {
+    return _handle.get();
+  }
+
+  OpusEncoder* handle() noexcept {
+    return _handle.get();
+  }
+
+  OpusEncoder* release() noexcept {
+    return _handle.release();
+  }
+
+  void reset(OpusEncoder* const handle) noexcept {
+    _handle.reset(handle);
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @see https://mf4.xiph.org/jenkins/view/opus/job/opus/ws/doc/html/group__opus__decoder.html
+ */
+class opus::decoder {
+public:
+  using handle_ptr = std::unique_ptr<OpusDecoder, void(*)(OpusDecoder*)>;
+
+protected:
+  // @see https://mf4.xiph.org/jenkins/view/opus/job/opus/ws/doc/html/group__opus__decoder.html#gafebf4cb3c29c9317cac385446a76e36e
+  handle_ptr _handle{nullptr, opus_decoder_destroy};
+
+public:
+  explicit decoder() noexcept = default;
+
+  explicit decoder(OpusDecoder* const handle) noexcept
+    : _handle{handle, opus_decoder_destroy} {}
+
+  const OpusDecoder* handle() const noexcept {
+    return _handle.get();
+  }
+
+  OpusDecoder* handle() noexcept {
+    return _handle.get();
+  }
+
+  OpusDecoder* release() noexcept {
+    return _handle.release();
+  }
+
+  void reset(OpusDecoder* const handle) noexcept {
+    _handle.reset(handle);
+  }
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 
